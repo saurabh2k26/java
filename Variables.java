a073a13c8bed711f4d9e0d07082598ceb29ab5c5
saurabh2k26 <saurabh2k26@gmail.com>
2020-08-02 11:36:57 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,26 @@
+public class Variables {
+  public static void main(String[] args){
+    int myAge;   // 32 bit
+    myAge = 35;
+    System.out.println(myAge);
+
+    short myShort; // 16 bit
+
+    long myLong; // 64 bit
+
+    double myHeight = 5.11 ;
+    float myHeightagain = 5.11f ; // float is a short version of double and you need to append it with 'f'
+    System.out.println(myHeight);
+    System.out.println(myHeightagain);
+
+    char myChar = 'p';  // unicode
+
+    boolean myBoolean = true;
+
+    byte myByte = 127 ;
+
+    String myName = "Saurabh";
+    System.out.println(myName);
+
+  }
+}
